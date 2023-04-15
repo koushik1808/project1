1c8169617865393cc31b3ce7c27b56acd3a98d0f
koushik1808 <koushikmandal189@gmail.com>
2023-04-15 13:13:17 +0530

    first commit

--- NiceAdmin/assets/vendor/chart.js/types.d.ts
+++ NiceAdmin/assets/vendor/chart.js/types.d.ts
@@ -2,9 +2,16 @@
  * Temporary entry point of the types at the time of the transition.
  * After transition done need to remove it in favor of index.ts
  */
-export * from './index.js';
-/**
- * Explicitly re-exporting to resolve the ambiguity.
- */
-export { BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, Animation, Animations, Chart, DatasetController, Interaction, Scale, Ticks, defaults, layouts, registry, ArcElement, BarElement, LineElement, PointElement, BasePlatform, BasicPlatform, DomPlatform, Decimation, Filler, Legend, SubTitle, Title, Tooltip, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, registerables } from './types/index.js';
-export * from './types/index.js';
+export * from './helpers.color.js';
+export * from './helpers.collection.js';
+export * from './helpers.core.js';
+export * from './helpers.curve.js';
+export * from './helpers.dom.js';
+export * from './helpers.easing.js';
+export * from './helpers.extras.js';
+export * from './helpers.interpolation.js';
+export * from './helpers.intl.js';
+export * from './helpers.math.js';
+export * from './helpers.options.js';
+export * from './helpers.rtl.js';
+export * from '../types/helpers/index.js';
